45e65b5f855f1161923a428490b069e1b0a4b122
TioaTyan <8370450+TioaTyan@users.noreply.github.com>
2019-11-28 23:35:03 +0800

    Project:Install Client-Side packages

--- CampusResourceSharingPlatform.Web/Startup.cs
+++ CampusResourceSharingPlatform.Web/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace CampusResourceSharingPlatform.Web
@@ -41,6 +43,11 @@ namespace CampusResourceSharingPlatform.Web
 			}
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
+			app.UseStaticFiles(new StaticFileOptions
+			{
+				RequestPath ="/node_modules",
+				FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "node_modules"))
+			});
 
 			app.UseRouting();
 
