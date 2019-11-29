a3a9d6575552b706b2daaf103eecbe0cfa1becab
TioaTyan <8370450+TioaTyan@users.noreply.github.com>
2019-11-29 17:06:36 +0800

    项目：注册、登录、关于 入口

---
+++ CampusResourceSharingPlatform.Web/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CampusResourceSharingPlatform.Web.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+	}
+}
