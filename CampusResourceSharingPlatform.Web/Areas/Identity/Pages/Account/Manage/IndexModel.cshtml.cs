e4efe2b7463f351f5941c06a515e5697b93de280
TioaTyan <xiaozhidiguo@gmail.com>
2020-01-15 23:39:36 +0800

    账户管理：头像上传

---
+++ CampusResourceSharingPlatform.Web/Areas/Identity/Pages/Account/Manage/IndexModel.cshtml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CampusResourceSharingPlatform.Web.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
+
+namespace CampusResourceSharingPlatform.Web.Areas.Identity.Pages.Account.Manage
+{
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IWebHostEnvironment _iWebHostEnvironment;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IWebHostEnvironment iWebHostEnvironment)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _iWebHostEnvironment = iWebHostEnvironment;
+        }
+
+        public string Username { get; set; }
+        public string AvatarFileName { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+
+            [Display(Name = "Avatar")]
+            public IFormFile NewAvatar { get; set; }
+        }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+
+            Input = new InputModel
+            {
+                PhoneNumber = phoneNumber
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+			var user = await _userManager.GetUserAsync(User);
+			//判断表达合法
+			if (!ModelState.IsValid)
+			{
+				await LoadAsync(user);
+				return Page();
+			}
+			//判断用户是否存在
+			if (user == null)
+			{
+				return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+			}
+
+			switch (id)
+			{
+				//表单类型判断
+				case "Avatar":
+				{
+					if (Input.NewAvatar != null)
+					{
+						var uploadFolder = Path.Combine(_iWebHostEnvironment.WebRootPath, "avatar", user.UserName);
+						AvatarFileName = Guid.NewGuid() + Path.GetExtension(Input.NewAvatar.FileName);
+						var filePath = Path.Combine(uploadFolder, AvatarFileName);
+						if (!Directory.Exists(uploadFolder))
+						{
+							Directory.CreateDirectory(uploadFolder);
+						}
+						await Input.NewAvatar.CopyToAsync(new FileStream(filePath, FileMode.Create));
+					}
+					try
+					{
+						user.ProfilePhotoUrl = "/avatar/" + user.UserName +"/" + AvatarFileName;
+						var result = await _userManager.UpdateAsync(user);
+						if (!result.Succeeded)
+						{
+							StatusMessage = "Error：图片上传失败，请重试。";
+						}
+						StatusMessage = "Success：图片上传成功。";
+					}
+					catch
+					{
+						StatusMessage = "Error：不可预料到的错误，请重试。";
+						return RedirectToPage();
+					}
+
+					break;
+				}
+				case "PhoneNumber":
+				{
+					var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+					if (Input.PhoneNumber != phoneNumber)
+					{
+						var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+						if (!setPhoneResult.Succeeded)
+						{
+							var userId = await _userManager.GetUserIdAsync(user);
+							throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
+						}
+					}
+					await _signInManager.RefreshSignInAsync(user);
+					StatusMessage = "Your profile has been updated";
+					return RedirectToPage();
+				}
+			}
+	        return RedirectToPage();
+        }
+    }
+}
