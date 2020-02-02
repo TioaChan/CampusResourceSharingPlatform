480de2cecee0eb483158a51dcaabea1cb9c9b45d
TioaTyan <tioatyan@outlook.com>
2020-02-02 18:17:11 +0800

    Update DbSeedInitializer.cs

--- CampusResourceSharingPlatform.Web/Data/DbSeedInitializer.cs
+++ CampusResourceSharingPlatform.Web/Data/DbSeedInitializer.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using CampusResourceSharingPlatform.Web.Models;
 
 namespace CampusResourceSharingPlatform.Web.Data
 {
-	public class DbSeedInitializer
+	public static class DbSeedInitializer
 	{
 		public static void DbSeedInitialize(ApplicationDbContext context)
 		{
