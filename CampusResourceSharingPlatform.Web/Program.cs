563740c4121d400ebc1ebf4438bcbf37c0fd56fb
TioaTyan <tioatyan@outlook.com>
2020-02-07 21:13:14 +0800

    use log4net

--- CampusResourceSharingPlatform.Web/Program.cs
+++ CampusResourceSharingPlatform.Web/Program.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -18,7 +13,17 @@ namespace CampusResourceSharingPlatform.Web
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
+
+			#region MyRegion
+			.ConfigureLogging((context, loggingBuilder) =>
+			{
+				loggingBuilder.AddFilter("System", LogLevel.Warning);//过滤掉命名空间
+				loggingBuilder.AddFilter("Microsoft", LogLevel.Warning);
+				loggingBuilder.AddLog4Net();//使用log4net
+			})//扩展日志
+			#endregion
+
+			.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
 				});
