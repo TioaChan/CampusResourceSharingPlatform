a33739863a8fba9b02ae203dbdfc19954922341f
TioaTyan <tioatyan@outlook.com>
2020-02-15 21:06:01 +0800

    manage ui

---
+++ CampusResourceSharingPlatform.Web/Controllers/ManageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CampusResourceSharingPlatform.Web.Controllers
+{
+	public class ManageController:Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Role()
+		{
+			return View();
+		}
+	}
+}
