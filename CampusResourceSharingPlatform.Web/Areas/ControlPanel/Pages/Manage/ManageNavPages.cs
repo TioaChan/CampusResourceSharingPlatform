8541206e0169c1df983c10a639cccfce0075ad53
TioaTyan <tioatyan@outlook.com>
2020-02-16 14:19:08 +0800

    ui complete

---
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/ManageNavPages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage
+{
+    public static class ManageNavPages
+    {
+        public static string Index => "Index";
+
+        public static string RoleIndex => "RoleIndex";
+
+        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+        public static string RoleIndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, RoleIndex);
+
+        private static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
--- CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Role/RoleNavPages.cs
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Role/RoleNavPages.cs
@@ -8,38 +8,18 @@ namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage.Role
 {
     public static class RoleNavPages
     {
-        public static string Index => "Index";
+	    public static string Index => "Index";
 
-        public static string Email => "Email";
+	    public static string RoleIndex => "RoleIndex";
 
-        public static string ChangePassword => "ChangePassword";
+	    public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+	    public static string RoleIndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, RoleIndex);
 
-        public static string ExternalLogins => "ExternalLogins";
-
-        public static string PersonalData => "PersonalData";
-
-        public static string TwoFactorAuthentication => "TwoFactorAuthentication";
-
-        public static string StudentIdentity => "StudentIdentity";
-
-        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
-
-        public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
-
-        public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
-
-        public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
-
-        public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
-
-        public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
-
-        public static string StudentIdentityClass(ViewContext viewContext) => PageNavClass(viewContext, StudentIdentity);
-        private static string PageNavClass(ViewContext viewContext, string page)
-        {
-            var activePage = viewContext.ViewData["ActivePage"] as string
-                ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
-            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
-        }
+	    private static string PageNavClass(ViewContext viewContext, string page)
+	    {
+		    var activePage = viewContext.ViewData["ActivePage"] as string
+		                     ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+		    return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+	    }
     }
 }
