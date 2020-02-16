2af8863a7074fc937c37008cc2f557212613185d
TioaTyan <tioatyan@outlook.com>
2020-02-16 14:40:46 +0800

    overview information

--- CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Index.cshtml.cs
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Index.cshtml.cs
@@ -1,16 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using CampusResourceSharingPlatform.Web.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
-namespace CampusResourceSharingPlatform.Web.Areas.ContrilPanel.Pages.Manage
+namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage
 {
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class IndexModel : PageModel
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public IndexModel(UserManager<ApplicationUser> userManager,RoleManager<IdentityRole> roleManager)
+		{
+			_userManager = userManager;
+			_roleManager = roleManager;
+		}
+		public int UserCount { get; set; }
+		public int RoleCount { get; set; }
+		public async Task<IActionResult> OnGetAsync()
+		{
+			UserCount =await _userManager.Users.CountAsync();
+			RoleCount = await _roleManager.Roles.CountAsync();
+			return Page();
+		}
+	}
 }
