4664ba8e07422ac62107a666d25d7d4110248922
TioaTyan <xiaozhidiguo@gmail.com>
2020-03-21 23:02:00 +0800

    Role and Users lists in ControlPanel Page

--- CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Role/Index.cshtml.cs
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Role/Index.cshtml.cs
@@ -2,15 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CampusResourceSharingPlatform.Model;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage.Role
 {
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class IndexModel : PageModel
+	{
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public List<IdentityRole> Roles { get; set; }
+
+
+		public IndexModel(RoleManager<IdentityRole> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+		public IActionResult OnGetAsync()
+		{
+			Roles= _roleManager.Roles.ToList();
+			return Page();
+		}
+	}
 }
--- CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Users/Index.cshtml.cs
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Users/Index.cshtml.cs
@@ -2,15 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CampusResourceSharingPlatform.Model;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage.Users
 {
-    public class IndexModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class IndexModel : PageModel
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public List<ApplicationUser> Roles { get; set; }
+
+
+		public IndexModel(UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
+		public IActionResult OnGetAsync()
+		{
+			Roles = _userManager.Users.ToList();
+			return Page();
+		}
+	}
 }
