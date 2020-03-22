5942dbde836daa039e5bef3265886e3c55e1e6c2
TioaTyan <xiaozhidiguo@gmail.com>
2020-03-22 18:21:06 +0800

    Add Role Index page for CP

--- CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Role/Index.cshtml.cs
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Role/Index.cshtml.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using CampusResourceSharingPlatform.Model;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,17 +11,41 @@ namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage.Role
 	{
 		private readonly RoleManager<IdentityRole> _roleManager;
 
-		public List<IdentityRole> Roles { get; set; }
+		public IEnumerable<IdentityRole> Roles { get; set; }
 
+		[TempData]
+		public string StatusMessage { get; set; }
 
 		public IndexModel(RoleManager<IdentityRole> roleManager)
 		{
 			_roleManager = roleManager;
 		}
-		public IActionResult OnGetAsync()
+
+		private async Task LoadRoles()
 		{
-			Roles= _roleManager.Roles.ToList();
+			Roles = await _roleManager.Roles.ToListAsync();
+		}
+
+		public async Task<IActionResult> OnGetAsync()
+		{
+			await LoadRoles();
 			return Page();
+		}
+
+		public async Task<IActionResult> OnPostDeleteRoleAsync(string id)
+		{
+			var role = await _roleManager.FindByIdAsync(id);
+			if (role!=null)
+			{
+				var result = await _roleManager.DeleteAsync(role);
+				StatusMessage = result.Succeeded ? "Success:角色删除成功" : "Error:角色删除失败";
+			}
+			else
+			{
+				StatusMessage = "Error:角色删除失败";
+			}
+			await LoadRoles();
+			return RedirectToPage();
 		}
 	}
 }
