dd783980cb861517eec3a662970feac3530f7699
TioaTyan <xiaozhidiguo@gmail.com>
2020-03-22 19:15:03 +0800

    Add add-role function for CP

---
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Role/Add.cshtml.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CampusResourceSharingPlatform.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage.Role
+{
+	public class AddModel : PageModel
+	{
+		private readonly RoleManager<IdentityRole> _roleManager;
+		public AddModel(RoleManager<IdentityRole> roleManager)
+		{
+			_roleManager = roleManager;
+		}
+
+		[TempData]
+		public string StatusMessage { get; set; }
+
+		[BindProperty]
+		public InputModel Input { get; set; }
+
+		public class InputModel
+		{
+			[Required]
+			[Display(Name = "Role Name")]
+			public string RoleName { get; set; }
+		}
+
+		public IActionResult OnGetAsync()
+		{
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAddRoleAsync()
+		{
+			var role=new IdentityRole 
+			{
+				Name = Input.RoleName
+			};
+			var result = await _roleManager.CreateAsync(role);
+			StatusMessage = result.Succeeded ? "Success:角色添加成功" : "Error:角色添加失败";
+			return RedirectToPage("Index");
+		}
+	}
+}
