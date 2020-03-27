38fa0137521395f42d585cb35b3a1cfe212aca15
TioaTyan <xiaozhidiguo@gmail.com>
2020-03-27 17:36:00 +0800

    Move applicationuser and license's model to folder

--- CampusResourceSharingPlatform.Model/Application/ApplicationUser.cs
+++ CampusResourceSharingPlatform.Model/Application/ApplicationUser.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
-namespace CampusResourceSharingPlatform.Model
+namespace CampusResourceSharingPlatform.Model.Application
 {
 	public class ApplicationUser:IdentityUser
 	{
