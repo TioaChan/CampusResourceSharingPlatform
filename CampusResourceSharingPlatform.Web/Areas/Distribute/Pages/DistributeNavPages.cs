9f893618ef0191e834513ffbf8bcc73d189e7575
TioaTyan <xiaozhidiguo@gmail.com>
2020-03-27 14:14:56 +0800

    Distribute pages' portal and demo

---
+++ CampusResourceSharingPlatform.Web/Areas/Distribute/Pages/DistributeNavPages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CampusResourceSharingPlatform.Web.Areas.Distribute.Pages
+{
+	public class DistributeNavPages
+	{
+		public static string Take => "Take";
+
+		public static string TakeOrder => "TakeOrder";
+
+		public static string Buy => "Buy";
+
+		public static string TakeNavClass(ViewContext viewContext) => PageNavClass(viewContext, Take);
+		public static string TakeOrderNavClass(ViewContext viewContext) => PageNavClass(viewContext, TakeOrder);
+
+		public static string BuyNavClass(ViewContext viewContext) => PageNavClass(viewContext, Buy);
+
+		private static string PageNavClass(ViewContext viewContext, string page)
+		{
+			var activePage = viewContext.ViewData["ActivePage"] as string
+							 ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+			return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+		}
+	}
+}
