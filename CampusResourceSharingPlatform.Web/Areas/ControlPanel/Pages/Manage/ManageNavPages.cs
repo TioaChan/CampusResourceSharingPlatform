c8e3c4a07d06c98ff4f19be70f012520194a085d
TioaTyan <xiaozhidiguo@gmail.com>
2020-04-02 13:58:52 +0800

    Post statistics portal in control panel page

--- CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/ManageNavPages.cs
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/ManageNavPages.cs
@@ -11,10 +11,12 @@ namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage
 
 		public static string UsersIndex => "UsersIndex";
 
+		public static string PostIndex => "PostIndex";
+
 		public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 		public static string RoleIndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, RoleIndex);
-
 		public static string UsersIndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, UsersIndex);
+		public static string PostIndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, PostIndex);
 
 		private static string PageNavClass(ViewContext viewContext, string page)
 		{
---
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Post/Index.cshtml.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CampusResourceSharingPlatform.Interface;
+using CampusResourceSharingPlatform.Model.Business;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage.Post
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ITakeExpressService<Express> _takeExpressService;
+		private readonly IPurchaseService<Purchase> _purchaseService;
+		private readonly IFleaMarketService<SecondHand> _fleaMarketService;
+		private readonly IHireService<Hire> _hireService;
+
+		public IndexModel(ITakeExpressService<Express> takeExpressService,
+			IPurchaseService<Purchase> purchaseService,
+			IFleaMarketService<SecondHand> fleaMarketService,
+			IHireService<Hire> hireService)
+		{
+			_takeExpressService = takeExpressService;
+			_purchaseService = purchaseService;
+			_fleaMarketService = fleaMarketService;
+			_hireService = hireService;
+		}
+
+		[TempData]
+		public string StatusMessage { get; set; }
+
+		public class TakeExpressPostCount
+		{
+			public string ActivePostCount { get; set; }
+			public string InvalidPostCount { get; set; }
+			public string DeletedPostCount { get; set; }
+		}
+		public class PurchasePostCount
+		{
+			public string ActivePostCount { get; set; }
+			public string InvalidPostCount { get; set; }
+			public string DeletedPostCount { get; set; }
+		}
+		public class FleaMarketPostCount
+		{
+			public string ActivePostCount { get; set; }
+			public string InvalidPostCount { get; set; }
+			public string DeletedPostCount { get; set; }
+		}
+		public class HirePostCount
+		{
+			public string ActivePostCount { get; set; }
+			public string InvalidPostCount { get; set; }
+			public string DeletedPostCount { get; set; }
+		}
+		public class PostCount
+		{
+			public TakeExpressPostCount TakeExpress { get; set; }
+
+			public PurchasePostCount Purchase { get; set; }
+
+			public FleaMarketPostCount FleaMarket { get; set; }
+
+			public HirePostCount HirePost { get; set; }
+		}
+
+		public PostCount Count { get; set; }
+
+		public async Task<IActionResult> OnGetAsync()
+		{
+			Count=new PostCount
+			{
+				TakeExpress = new TakeExpressPostCount
+				{
+					ActivePostCount = _takeExpressService.GetAllActiveMissionAsync().Result.Count.ToString(),
+					InvalidPostCount=_takeExpressService.GetAllInvalidMissionAsync().Result.Count.ToString(),
+					DeletedPostCount = _takeExpressService.GetAllDeletedMissionAsync().Result.Count.ToString()
+				},
+				Purchase =new PurchasePostCount
+				{
+				
+				},
+				FleaMarket = new FleaMarketPostCount
+				{
+				
+				},
+				HirePost = new HirePostCount
+				{
+				
+				},
+			};
+			return Page();
+		}
+	}
+}
