11de18eff1cbe5ac68732d2d11142781d7005de5
TioaTyan <xiaozhidiguo@gmail.com>
2020-04-03 16:25:53 +0800

    Fix Layout

--- CampusResourceSharingPlatform.Web/Areas/Posts/Pages/TakeExpress.cshtml.cs
+++ CampusResourceSharingPlatform.Web/Areas/Posts/Pages/TakeExpress.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CampusResourceSharingPlatform.Web.Areas.Posts.Pages
 {
