1d9149ba421419509414b534a15987b8f998fa0f
TioaTyan <xiaozhidiguo@gmail.com>
2020-04-08 15:06:21 +0800

    ?

--- CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Index.cshtml.cs
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Index.cshtml.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using CampusResourceSharingPlatform.Interface;
+using CampusResourceSharingPlatform.Model.Business;
 
 namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage
 {
@@ -13,18 +15,37 @@ namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage
 	{
 		private readonly UserManager<ApplicationUser> _userManager;
 		private readonly RoleManager<IdentityRole> _roleManager;
+		private readonly ITakeExpressService<Express> _takeExpressService;
+		private readonly IPurchaseService<Purchase> _purchaseService;
+		private readonly IFleaMarketService<SecondHand> _fleaMarketService;
+		private readonly IHireService<Hire> _hireService;
 
-		public IndexModel(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+		public IndexModel(UserManager<ApplicationUser> userManager,
+			RoleManager<IdentityRole> roleManager,
+			ITakeExpressService<Express> takeExpressService,
+			IPurchaseService<Purchase> purchaseService,
+			IFleaMarketService<SecondHand> fleaMarketService,
+			IHireService<Hire> hireService)
 		{
 			_userManager = userManager;
 			_roleManager = roleManager;
+			_takeExpressService = takeExpressService;
+			_purchaseService = purchaseService;
+			_fleaMarketService = fleaMarketService;
+			_hireService = hireService;
 		}
 		public int UserCount { get; set; }
 		public int RoleCount { get; set; }
+
+		public int PostsCount { get; set; }
 		public async Task<IActionResult> OnGetAsync()
 		{
 			UserCount = await _userManager.Users.CountAsync();
 			RoleCount = await _roleManager.Roles.CountAsync();
+			PostsCount = (await _takeExpressService.GetAllActiveMissionAsync()).Count
+			             + (await _purchaseService.GetAllActiveMissionAsync()).Count
+			             + (await _fleaMarketService.GetAllActiveMissionAsync()).Count
+			             + (await _hireService.GetAllActiveMissionAsync()).Count;
 			return Page();
 		}
 	}
--- CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Users/Index.cshtml.cs
+++ CampusResourceSharingPlatform.Web/Areas/ControlPanel/Pages/Manage/Users/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CampusResourceSharingPlatform.Interface;
+using CampusResourceSharingPlatform.Model.Business;
 
 namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage.Users
 {
@@ -14,21 +16,60 @@ namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage.User
 	public class IndexModel : PageModel
 	{
 		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly ITakeExpressService<Express> _takeExpressService;
+		private readonly IPurchaseService<Purchase> _purchaseService;
+		private readonly IFleaMarketService<SecondHand> _fleaMarketService;
+		private readonly IHireService<Hire> _hireService;
 
 		public IEnumerable<ApplicationUser> Users { get; set; }
+
+		public List<UserInformations> UserPackage { get; set; }
+		public class UserInformations
+		{
+			public ApplicationUser User { get; set; }
+			public int PostCount { get; set; }
+
+			public int ExpressPostCount { get; set; }
+
+			public int PurchasePostCount { get; set; }
+
+			public int FleaMarketPostCount { get; set; }
+
+			public int HireMarketPostCount { get; set; }
+		}
 
 		[TempData]
 		public string StatusMessage { get; set; }
 
-		public IndexModel(UserManager<ApplicationUser> userManager)
+		public IndexModel(UserManager<ApplicationUser> userManager, 
+			ITakeExpressService<Express> takeExpressService,
+			IPurchaseService<Purchase> purchaseService,
+			IFleaMarketService<SecondHand> fleaMarketService,
+			IHireService<Hire> hireService)
 		{
 			_userManager = userManager;
+			_takeExpressService = takeExpressService;
+			_purchaseService = purchaseService;
+			_fleaMarketService = fleaMarketService;
+			_hireService = hireService;
 		}
 
 		private async Task LoadUsersAsync()
 		{
-			//Users = _userManager.Users.ToList().Where(p => p.DeletedMark = false);
-			Users = await _userManager.Users.Where(p => p.DeletedMark == false).ToListAsync();
+			Users = await _userManager.Users.OrderBy(p=>p.DeletedMark).ToListAsync();
+			UserPackage = new List<UserInformations>();
+			foreach (var user in Users)
+			{
+				var userInformation = new UserInformations
+				{
+					User = user,
+					ExpressPostCount = (await _takeExpressService.GetAllActiveMissionByPostUserAsync(user)).Count,
+					PurchasePostCount = (await _purchaseService.GetAllActiveMissionByPostUserAsync(user)).Count,
+					FleaMarketPostCount = (await _fleaMarketService.GetAllActiveMissionByPostUserAsync(user)).Count,
+					HireMarketPostCount = (await _hireService.GetAllActiveMissionByPostUserAsync(user)).Count,
+				};	
+				UserPackage.Add(userInformation);
+			}
 		}
 
 		public async Task<IActionResult> OnGetAsync()
@@ -42,6 +83,15 @@ namespace CampusResourceSharingPlatform.Web.Areas.ControlPanel.Pages.Manage.User
 			user.DeletedMark = true;
 			var result = await _userManager.UpdateAsync(user);
 			StatusMessage = result.Succeeded ? "Success：用户删除成功。" : "Error：用户删除失败。";
+			await LoadUsersAsync();
+			return Page();
+		}
+		public async Task<IActionResult> OnPostRestoreUserAsync(string id)
+		{
+			var user = await _userManager.FindByIdAsync(id);
+			user.DeletedMark = false;
+			var result = await _userManager.UpdateAsync(user);
+			StatusMessage = result.Succeeded ? "Success：用户恢复成功。" : "Error：用户恢复失败。";
 			await LoadUsersAsync();
 			return Page();
 		}
